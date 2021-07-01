f76a364fbb565c2ee8c387c676c0aeda95c21319
Laura Tender <lau.tender@gmail.com>
2021-07-01 16:43:25 +0300

    Added 2 routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,38 @@ import './App.css';
 import IntroNasa from './IntroNasa';
 import AppContent from './AppContent';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          <IntroNasa/>
-          <AppContent/>
-      </header>
-    </div>
+    <Router>
+      <div className="App-header">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/button">Button Click Counter</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/about">
+            <IntroNasa/>
+          </Route>
+          <Route path="/button">
+            <AppContent/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
